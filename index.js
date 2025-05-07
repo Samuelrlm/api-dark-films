7ff1a9e4197d400c0ac8af95cf3aefa62e907c60
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-05-07 11:15:00 -0300

    cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const dotenv =  require('dotenv');
 const sequelize = require('./src/config/database.js');
 const movieRoutes = require('./src/routes/movieRoutes.js');
 const { errorHandler } = require('./src/middlewares/errorHandler.js');
-
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', movieRoutes);
